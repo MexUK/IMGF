de8c196eb447d1ead472ce469806ddffdbc3573a
Mex <mexuk@hotmail.co.uk>
2017-09-01 18:39:05 +0100

    Timers

--- Code/IMGF/Program/buildnumber.h
+++ Code/IMGF/Program/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 8943
-#define BUILDNUMBER_STR "8943" 
+#define BUILDNUMBER 9008
+#define BUILDNUMBER_STR "9008" 
 
 #endif /* BUILD_NUMBER_H_ */
